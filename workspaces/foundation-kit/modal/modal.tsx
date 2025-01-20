69f96ae54e35f8af37a14c857c5e5dcab225ee3e
Alexander <aosipov@inno.tech>
2025-01-21 03:31:29 +0400

    modal fix

--- workspaces/foundation-kit/modal/modal.tsx
+++ workspaces/foundation-kit/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { useIsMobile, useOnClickOutside } from "@hooks";
+import { useIsMobile, useMountTransition, useOnClickOutside } from "@hooks";
 import { FunctionalComponent } from "preact";
 import { useEffect, useRef } from "preact/hooks";
 import { CloseIcon } from "../icons/close";
@@ -16,6 +16,7 @@ export const Modal: FunctionalComponent<Modal> = ({
 }) => {
   const contentRef = useRef(null);
   const isMobile = useIsMobile();
+  const hasTransitionedIn = useMountTransition(isOpen, 400);
   const handleModalClose = () => {
     document.body.style.overflow = "visible";
     onClose();
@@ -30,7 +31,15 @@ export const Modal: FunctionalComponent<Modal> = ({
   return (
     <ModalBox style={{ visibility: isOpen ? "visible" : "hidden" }}>
       <Backdrop />
-      <Content ref={contentRef}>
+      <Content
+        style={
+          hasTransitionedIn && {
+            transform: isMobile && "translateY(0)",
+            opacity: !isMobile && 1,
+          }
+        }
+        ref={contentRef}
+      >
         {children}
         <IconBox onClick={handleModalClose}>
           <CloseIcon />
