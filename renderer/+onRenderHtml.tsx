247f75cfbc0966eae0035e79aa116985636a04e9
Alexander <aosipov@inno.tech>
2025-02-19 16:25:54 +0400

    add script ym

--- renderer/+onRenderHtml.tsx
+++ renderer/+onRenderHtml.tsx
@@ -54,6 +54,24 @@ async function onRenderHtml(pageContext) {
     })(window);</script>`
       : "";
 
+  const ymScript = `
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(99975606, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/99975606" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+`;
+
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html lang="ru">
       <head>
@@ -69,6 +87,9 @@ async function onRenderHtml(pageContext) {
         ${dangerouslySkipEscape(reservationScript)}
       </head>
       <body>
+      <!-- Yandex.Metrika counter -->
+        ${dangerouslySkipEscape(ymScript)}
+        <!-- /Yandex.Metrika counter -->
         ${dangerouslySkipEscape(pageHtml)}
       </body>
     </html>`;
