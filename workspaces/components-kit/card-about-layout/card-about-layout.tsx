8f61108aa877953e9aaac6b2374c5f8339b1d674
Alexander Osipov <aosipov@yandex.ru>
2025-07-19 11:59:08 +0400

    вмешает

--- workspaces/components-kit/card-about-layout/card-about-layout.tsx
+++ workspaces/components-kit/card-about-layout/card-about-layout.tsx
@@ -1,21 +1,6 @@
-import { FunctionComponent, toChildArray } from "preact";
-import {
-  CardAboutLayoutBox,
-  StyledButton,
-  WithButtonContainer,
-} from "./card-about-layout.styles";
+import { FunctionComponent } from "preact";
+import { CardAboutLayoutBox } from "./card-about-layout.styles";
 
 export const CardAboutLayout: FunctionComponent = ({ children }) => {
-  const childrens = toChildArray(children);
-  return (
-    <CardAboutLayoutBox>
-      {childrens[0]}
-      {childrens[1]}
-
-      <WithButtonContainer>
-        {childrens[2]}
-        <StyledButton>Забронировать номер</StyledButton>
-      </WithButtonContainer>
-    </CardAboutLayoutBox>
-  );
+  return <CardAboutLayoutBox>{children}</CardAboutLayoutBox>;
 };
