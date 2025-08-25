9ce44ecbdc53168d31bf967e0444a7ea1120e2db
Alexander Osipov <aosipov@yandex.ru>
2025-08-25 20:22:56 +0400

    метрика

--- renderer/+onRenderHtml.tsx
+++ renderer/+onRenderHtml.tsx
@@ -57,7 +57,7 @@ async function onRenderHtml(pageContext) {
   const ymScript = `
 <script type="text/javascript">
     (function(m,e,t,r,i,k,a){
-        m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
@@ -66,6 +66,7 @@ async function onRenderHtml(pageContext) {
     ym(99990893, 'init', {webvisor:true, clickmap:true, accurateTrackBounce:true, trackLinks:true});
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/99990893" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
 `;
 
   const documentHtml = escapeInject`<!DOCTYPE html>
